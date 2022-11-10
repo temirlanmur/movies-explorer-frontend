5abbb48be0f34e09d05e937c04a38caec381b371
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2022-11-11 00:35:10 +0600

    feat: add Navigation component without burger menu

---
+++ src/components/App/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import Navigation from '../Navigation';
+
+export default function App() {
+  return (
+    <Navigation isLoggedIn={true}/>
+  );
+};
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,26 @@
+import '../Utility/Button/Button.css';
+import '../Utility/Link/Link.css';
+import '../Utility/Logo/Logo.css';
+import './Navigation.css';
+
+export default function Navigation({ isLoggedIn }) {
+ return (
+    <div className="navigation">
+      <div className="logo navigation__logo"></div>
+      {
+        isLoggedIn
+        ?
+        <button class="navigation__hamburger">
+          <span className="navigation__hamburger-bar"></span>
+          <span className="navigation__hamburger-bar"></span>
+          <span className="navigation__hamburger-bar"></span>
+        </button>
+        :
+        (<>
+          <button class="link navigation__register-link">Регистрация</button>
+          <button class="button button_style_primary">Войти</button>
+        </>)
+      }
+    </div>
+ );
+};
