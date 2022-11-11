0d050078f5a40b2b271707258b76fca68ce04252
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2022-11-12 00:12:18 +0600

    feat: add Techs section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import './Main.css';
 import Promo from './Promo';
 import AboutProject from './AboutProject';
+import Techs from './Techs';
 
 export default function Main() {
   return (
     <main className="main">
       <Promo />
       <AboutProject />
+      <Techs />
     </main>
   );
 };
