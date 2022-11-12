520d0aae20286b8a33e76cfb2bcec022a208da54
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2022-11-12 18:05:59 +0600

    feat: add article blocks

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -1,3 +1,4 @@
+import '../../Utility/Article/Article.css';
 import '../../Utility/SectionHeading/SectionHeading.css';
 import '../../Utility/Timeline/Timeline.css';
 import './AboutProject.css';
@@ -7,20 +8,20 @@ export default function AboutProject() {
     <section className="about-project">
       <h2 className="section-heading">О проекте</h2>
       <hr className="section-heading__line" />
-      <article className="about-project__article">
-        <h3 className="about-project__subheading">
+      <article className="article about-project__article">
+        <h3 className="article__heading">
           Дипломный проект включал 5 этапов
         </h3>
-        <p className="about-project__paragraph">
+        <p className="article__paragraph">
           Составление плана, работу над бэкендом, вёрстку, добавление
           функциональности и финальные доработки.
         </p>
       </article>
-      <article className="about-project__article">
-        <h3 className="about-project__subheading">
+      <article className="article about-project__article">
+        <h3 className="article__heading">
           На выполнение диплома ушло 5 недель
         </h3>
-        <p className="about-project__paragraph">
+        <p className="article__paragraph">
           У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
           соблюдать, чтобы успешно защититься.
         </p>
