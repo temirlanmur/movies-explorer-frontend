a70768cf84f1740a9f55407fa1156efc1fe9d48d
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2022-11-14 17:22:05 +0600

    feat: add SearchForm component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,9 +1,12 @@
+import SearchForm from '../SearchForm/SearchForm';
+
 import '../Utility/Button/Button.css';
 import './Movies.css';
 
 export default function Movies() {
   return (
     <main className="movies">
+      <SearchForm />
       <button className="button movies__button">
         Ещё
       </button>
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,26 @@
+import '../Utility/Button/Button.css';
+import './SearchForm.css';
+
+export default function SearchForm() {
+  return (
+    <section className="search-form">
+      <form className="search-form__form">
+        <label className="search-form__text-label">
+          <input
+            className="search-form__text-input"
+            type="text"
+            placeholder="Фильм"
+          />
+          <button className="button search-form__button" type="submit">
+            Найти
+          </button>
+        </label>
+        <label className="search-form__switch-label">
+          <input className="search-form__switch-input" type="checkbox" />
+          <span className="search-form__switch" />
+          Короткометражки
+        </label>
+      </form>
+    </section>
+  );
+};
