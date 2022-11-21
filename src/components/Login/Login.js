ef217c1feee43b44b8f173c2cedbb23249f45097
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2022-11-22 01:39:20 +0600

    feat: add state & validation to Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,35 +1,74 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import validator from 'validator';
+
 import '../Utility/Button/Button.css';
 import '../Utility/Link/Link.css';
 import '../Utility/Logo/Logo.css';
 
 import '../Utility/Auth/Auth.css';
-import { Link } from 'react-router-dom';
+
+import { AuthTextInput } from '../Utility/Auth';
 
 export default function Login() {
+
+  const [email, setEmail] = useState({ value: '', error: '' });
+  const [password, setPassword] = useState({ value: '', error: '' });
+
+  function handleEmailChange(event) {
+    const { value } = event.target;
+    let error = '';
+    if (!value) {
+      error = 'Поле не должно быть пустым';
+    }
+    if (!validator.isEmail(value)) {
+      error = 'Невалидный email';
+    }
+    setEmail({ value, error });
+  }
+
+  function handlePasswordChange(event) {
+    const { value } = event.target;
+    let error = '';
+    if (!value) {
+      error = 'Поле не должно быть пустым';
+    }
+    setPassword({ value, error });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className="auth">
       <div className="auth__wrapper">
         <div className="logo auth__logo"></div>
         <h1 className="auth__heading">Рады видеть!</h1>
-        <form className="auth__form">
+        <form className="auth__form" onSubmit={handleSubmit} noValidate={true}>
           <label className="auth__label">
             E-mail
-            <input
-              className="auth__input"
+            <AuthTextInput
+              name="login-email"
               type="email"
-              placeholder="my-email@mail.com"
-              required
+              placeholder="jimmy-goggins@email.com"
+              required={true}
+              value={email.value}
+              onChange={handleEmailChange}
+              errorText={email.error}
             />
-            <span className="auth__error-text"></span>
           </label>
           <label className="auth__label">
             Пароль
-            <input
-              className="auth__input"
+            <AuthTextInput
+              name="login-password"
               type="password"
-              required
+              placeholder=""
+              required={true}
+              value={password.value}
+              onChange={handlePasswordChange}
+              errorText={password.error}
             />
-            <span className="auth__error-text"></span>
           </label>
         </form>
         <button className="button auth__button" type="submit">
